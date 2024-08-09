aa27b502772d18bd5b25aa7329180433b5aa24a2
gfmota <gabrielfmota134@gmail.com>
2024-08-08 21:34:02 -0300

    initialize service
    Signed-off-by: gfmota <gabrielfmota134@gmail.com>

---
+++ src/main/java/evolvability/thesis/injest_service/InjestServiceApplication.java
@@ -0,0 +1,13 @@
+package evolvability.thesis.injest_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InjestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InjestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/evolvability/thesis/injest_service/InjestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package evolvability.thesis.injest_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InjestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
