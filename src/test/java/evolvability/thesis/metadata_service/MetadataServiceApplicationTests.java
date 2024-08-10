b094eecb7de3255a428c4e9768eb1850e9408bd3
gfmota <gabrielfmota134@gmail.com>
2024-08-10 20:01:48 -0300

    initialize app
    Signed-off-by: gfmota <gabrielfmota134@gmail.com>

---
+++ src/test/java/evolvability/thesis/metadata_service/MetadataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package evolvability.thesis.metadata_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetadataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
