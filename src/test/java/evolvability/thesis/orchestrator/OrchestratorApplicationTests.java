978eee6d85450005bf835a620761799ce15d3169
gfmota <gabrielfmota134@gmail.com>
2024-09-08 12:16:51 -0300

    first commit

---
+++ src/test/java/evolvability/thesis/orchestrator/OrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package evolvability.thesis.orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
