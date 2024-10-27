67ede7743e9d383ec7db26f0fd44835158134eac
gfmota <gabrielfmota134@gmail.com>
2024-10-27 15:49:17 -0300

    add wind temperature
    Signed-off-by: gfmota <gabrielfmota134@gmail.com>

--- src/main/java/evolvability/thesis/austrian_geosphere_data_collector/infrastructure/clients/GeosphereClient.java
+++ src/main/java/evolvability/thesis/austrian_geosphere_data_collector/infrastructure/clients/GeosphereClient.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 @FeignClient(name = "geosphere-service", url = "${geosphere-service.url}")
 public interface GeosphereClient {
-    @GetMapping("/v1/station/current/tawes-v1-10min?parameters=P,FF,DD&output_format=geojson")
+    @GetMapping("/v1/station/current/tawes-v1-10min?parameters=P,FF,DD,TL&output_format=geojson")
     Object getAirPressureCurrentStatus(@RequestParam("station_ids") List<String> stationIds);
 }
